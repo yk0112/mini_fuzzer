2f611b0b5449690194a8dca55ad663d727f32031
yk0112 <yosikou0726@gmail.com>
2023-12-29 03:34:52 +0900

    feat: add mutation part2

--- mini_fuzzer/fuzz.py
+++ mini_fuzzer/fuzz.py
@@ -5,21 +5,21 @@ import subprocess
 import sys
 import threading
 import time
-from typing import List
+from itertools import product
 
 sys.path.append("../mini_fuzzer/mutation.py")
 sys.path.append("../mini_fuzzer/monitor.py")
 
+import mutation
 from monitor import Monitor
-from mutation import bitflip
 
 lock: threading.Lock = threading.Lock()
 
 
 class Fuzzer:
     target: str
-    seeds: queue.Queue[List[str]]
-    testing_seed: List[str]
+    seeds: queue.Queue[list[str]]
+    testing_seed: list[str]
     rand: random.SystemRandom
 
     def __init__(self, target: str, seeds: queue.Queue) -> None:
@@ -29,29 +29,28 @@ class Fuzzer:
 
     def do_fuzzing(self) -> int:
         seed = self.seeds.get()
-        self.testing_seed = seed
-        seed.insert(0, self.target)
-        command = " ".join(seed)
+        self.testing_seed = seed[:]
+        args = []
+        for s in seed:
+            arg = "'" + s + "'"
+            args.append(arg)
+
+        args.insert(0, self.target)
+        command = " ".join(args)
         child_process = subprocess.run(
             command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True
         )
-        
         status = child_process.returncode
         return status
     
-    def mutation(self, s:str)->str:
-        mutators = [
-            bitflip
-        ]
-        mutator = random.choice(mutators)
-        return mutator(s)
+    def mutation_fuzz(self) -> None:
+        seed = self.testing_seed[:]
+        mutators = [mutation.delete_random_character]
+        
+        for mutator_combination in product(mutators, repeat=len(seed)):
+            result = [func(elem) for func, elem in zip(mutator_combination, seed)]
+            self.seeds.put(result)
     
-    def generate_fuzz(self) -> None:
-        mutated_seed = []
-        for s in self.testing_seed:
-            mutated_seed.append(self.mutation(s))
-        self.seeds.put(mutated_seed)
-
 
     def dump(self, status: int) -> None:
         fuzz_and_status = str(status) + "," + " ".join(self.testing_seed) + "\n"
@@ -62,7 +61,9 @@ class Fuzzer:
 
 def display_run_time(stop_event, monitor) -> None:
     while not stop_event.is_set():
+        lock.acquire()
         monitor.display()
+        lock.release()
         time.sleep(1)
 
 
@@ -82,25 +83,30 @@ def main():
     monitor = Monitor()
     
     stop_event = threading.Event()
-    thread = threading.Thread(target=display_run_time, args=(stop_event, monitor))
+    thread = threading.Thread(target=display_run_time, args=(stop_event, monitor), daemon=True)
     thread.start()
 
     try:
         while not fuzzer.seeds.empty():
             lock.acquire()
             monitor.cycles_done += 1
+            monitor.display()
             lock.release()
             status = fuzzer.do_fuzzing()
             if status > 0:
                 lock.acquire()
                 monitor.uniqu_crash += 1
                 monitor.last_crash_time = time.time()
-                lock.release()
                 monitor.display()
-
-                if True:  # for test
-                   fuzzer.generate_fuzz()
+                lock.release()
                 fuzzer.dump(status)
+            else:
+                fuzzer.dump(0) # for test
+            if True:  # for test
+               fuzzer.mutation_fuzz()
+
+        print("\033[10B\n")
+        stop_event.set()
     except KeyboardInterrupt:
         print("\033[10B\n")
         stop_event.set()
--- mini_fuzzer/mutation.py
+++ mini_fuzzer/mutation.py
@@ -1,4 +1,10 @@
 import random
+import string
+import sys
+
+from config import DECREMENT_VALUE, INCREMENT_VALUE, INTERESTING_VALUE
+
+sys.path.append("../mini_fuzzer/config.py")
 
 
 # Flip a random bit in a random character
@@ -7,9 +13,72 @@ def bitflip(s: str) -> str:
         return ""
 
     rand = random.SystemRandom()
-    chr_position = rand.randint(0, len(s))
-    origin_chr = s[chr_position]
-    bit_position = rand.randint(0, ord(origin_chr).bit_length())
-    fliped_chr = chr(ord(origin_chr) ^ (1 << bit_position))
+    chr_pos = rand.randint(0, len(s) - 1)
+    origin_chr = s[chr_pos]
+    bit_pos = rand.randint(0, ord(origin_chr).bit_length() - 1)
+    fliped_chr = chr(ord(origin_chr) ^ (1 << bit_pos))
+    if not fliped_chr.isprintable() or ord(fliped_chr) == 0: # prevent control character and null character
+        return s
+    else: 
+        return s[:chr_pos] + fliped_chr + s[chr_pos + 1 :]
 
-    return s[:chr_position] + fliped_chr + s[chr_position + 1 :]
+# Flip a random character
+def byteflip(s:str) -> str:
+    if s == "":
+        return ""
+
+    rand = random.SystemRandom()
+    chr_pos = rand.randint(0, len(s) - 1)
+    fliped_chr = chr(ord(s[chr_pos]) ^ 0xFF)
+    if not fliped_chr.isprintable() or ord(fliped_chr) == 0: 
+        return s
+    else: 
+        return s[:chr_pos] + fliped_chr + s[chr_pos + 1 :]
+
+
+# Increase by a constant value
+def arithmetic_inc(s: str) -> str:
+    num = 0
+    if s.isdigit():
+       num = int(s)
+    else:
+        try:
+            num = float(s)
+        except ValueError:
+            return s
+    return str(num + INCREMENT_VALUE)        
+
+
+ # Decrease by a constant value
+def arithmetic_dec(s: str) -> str:
+    num = 0
+    if s.isdigit():
+       num = int(s)
+    else:
+        try:
+            num = float(s)
+        except ValueError:
+            return s
+    return str(num - DECREMENT_VALUE)        
+   
+
+# Return a interesting value 
+def add_interesting_value(s: str) -> str:
+    return str(random.choice(INTERESTING_VALUE))
+
+
+# Add a random character at random location
+def add_random_character(s: str) -> str:
+    random_char = random.choice(string.printable)
+    insert_pos = random.randint(0, len(s))
+    return s[:insert_pos] + random_char + s[insert_pos:]
+
+
+# Delete a random character at random location
+def delete_random_character(s: str) -> str:
+    if s == "":
+        return s
+    delete_pos = random.randint(0, len(s) - 1)
+    return s[:delete_pos] + s[delete_pos + 1:]
+
+
